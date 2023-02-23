b4217696cfd2ae58fa46e43dcefb10ef226ff480
Adem AKPINAR <ademakpinar.bt@gmail.com>
2023-02-23 20:42:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,69 +1,124 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import React, { useState } from "react";
 
-import api from "./api/api";
-import urls from "./api/urls";
-import { useDispatch, useSelector } from "react-redux";
-import actionTypes from "./redux/actions/actionTypes";
-import PersonDetail from "./pages/PersonDetail";
-import AddPerson from "./pages/AddPerson";
-import EditPerson from "./pages/EditPerson";
+import Header from "../components/Header";
 
-function App() {
+import { useParams, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import api from "../api/api";
+import urls from "../api/urls";
+
+import actionTypes from "../redux/actions/actionTypes";
+
+const EditPerson = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const params = useParams();
   const { personsState, groupsState } = useSelector((state) => state);
-
-  useEffect(() => {
-    /*fetch persons */
-    dispatch({ type: actionTypes.personActions.GET_PERSONS_START });
-    api
-      .get(urls.persons)
-      .then((res) => {
-        dispatch({
-          type: actionTypes.personActions.GET_PERSONS_SUCCESS,
-          payload: res.data,
-        });
-      })
-      .catch((err) => {
-        dispatch({
-          type: actionTypes.personActions.GET_PERSONS_FAIL,
-          payload: "Serverda hata oluştu",
-        });
-      });
-
-    /*fetch groups */
-    dispatch({ type: actionTypes.groupActions.GET_GROUPS_START });
-    api
-      .get(urls.groups)
-      .then((res) => {
-        dispatch({
-          type: actionTypes.groupActions.GET_GROUPS_SUCCESS,
-          payload: res.data,
-        });
-      })
-      .catch((err) => {
-        dispatch({
-          type: actionTypes.groupActions.GET_GROUPS_FAIL,
-          payload: "Serverda hata oluştu",
-        });
-      });
-  }, []);
-
-  if (personsState.success === false || groupsState.success === false)
-    return null;
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="person-detail/:personId" element={<PersonDetail />} />
-        <Route path="add-person" element={<AddPerson />} />
-        <Route path="edit-person/:personId" element={<EditPerson />} />
-      </Routes>
-    </BrowserRouter>
+  const myPerson = personsState.persons.find(
+    (item) => item.id === params.personId
   );
-}
+  //console.log(myBook);
+  const [form, setForm] = useState(myPerson);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    /* validation */
+    if (form.name === "" || form.surname === "" || form.groupsId === "") {
+      alert("Kitap adı, Yazar alanı ve kategori alanı zorunludur");
+      return;
+    }
+    if (form.name.length < 2) {
+      alert("Kitap ismi 2 karakterden az olamaz");
+      return;
+    }
+    api
+      .put(`${urls.persons}/${params.personId}`, form)
+      .then((res) => {
+        dispatch({
+          type: actionTypes.personActions.EDIT_PERSON,
+          payload: form,
+        });
+        navigate("/");
+      })
+      .catch((err) => {});
+  };
+  return (
+    <div>
+      <Header />
+      <div className="container my-5">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Kitap Adı
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Yalnızız"
+              value={form.name}
+              onChange={(event) =>
+                setForm({ ...form, name: event.target.value })
+              }
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="surname" className="form-label">
+              Yazar
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="surname"
+              placeholder="Peyami Safa"
+              value={form.surname}
+              onChange={(event) =>
+                setForm({ ...form, surname: event.target.value })
+              }
+            />
+          </div>
 
-export default App;
+          <div className="mb-3">
+            <label htmlFor="number" className="form-label">
+              Fiyatı
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="number"
+              placeholder="69.70"
+              value={form.number}
+              onChange={(event) =>
+                setForm({ ...form, number: Number(event.target.value) })
+              }
+            />
+          </div>
+
+          <select
+            className="form-select"
+            defaultValue={groupsState.groups[0].id}
+            value={form.groupId}
+            onChange={(event) =>
+              setForm({ ...form, groupId: event.target.value })
+            }
+          >
+            {groupsState.groups.map((item) => (
+              <option key={item.id} value={item.id}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+
+          <div className="d-flex justify-content-center my-5">
+            <button className="btn btn-primary w-50" type="submit">
+              Güncelle
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditPerson;
 
